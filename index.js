b2709e4a266ae2f7a4f1ba51a4d7186c7d106102
Karthik <karthikalakunta21@gmail.com>
2022-07-20 14:03:19 +0530

    added homepage files

---
+++ index.js
@@ -0,0 +1,10 @@
+let sales1=document.querySelector("#sale");
+sales1.addEventListener("click",salefunc);
+function salefunc(){
+    window.location.href="homepage1.html"
+}
+let sales=document.querySelector(".home-page-1");
+sales.addEventListener("click",salesfunc);
+function salesfunc(){
+    window.location.href="homepage1.html"
+}
