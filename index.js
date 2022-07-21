8906d35432174e8f2050a730ca41b58491407d59
Karthik <karthikalakunta21@gmail.com>
2022-07-21 18:26:02 +0530

    added products file and data of the products cart total file also added

--- index.js
+++ index.js
@@ -144,16 +144,12 @@ function divf203func(){
 let signin=document.querySelector("#signin");
 let signup=document.querySelector("#signup");
 let signinarr=JSON.parse(localStorage.getItem("signin"))||[];
-if(signinarr !== null){
+if(signinarr === null){
+    signin.textContent="Sign In";
+    signup.textContent="Sign Up"
+}
+else if(signinarr !== null){
     signin.textContent=signinarr[signinarr.length-1].name;
-    signup.textContent="Log out"
+    signup.textContent=""
 }
-signup.addEventListener("click",signupfunc);
-function signupfunc(){
-    if(signup.textContent==="Log out"){
-        window.location.href="signup.html"
-    }else{
-        signinarr.spice(signinarr.length-1,1);
-       
-    }
-}
+
