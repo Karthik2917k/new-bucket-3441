6e18cec9d81837019eca9c1f92b95608185c71cf
Karthik <karthikalakunta21@gmail.com>
2022-07-22 16:44:18 +0530

    added transaction pages small things modifyed in all html files

---
+++ otp.js
@@ -0,0 +1,26 @@
+let form=document.querySelector("form");
+form.addEventListener("submit",otpfunc);
+let button=document.querySelector("#button");
+button.addEventListener("click",cancelfunc);
+function cancelfunc(){
+    alert("your redirected to card page");
+    window.location.href="cart.html"
+}
+function otpfunc(e){
+    e.preventDefault();
+    let otp=form.otp.value;
+    let obj={
+        Otp:otp,
+    }
+    if(otp===""){
+        alert("Plese Enter OTP")
+    }else{
+        if(otp==="1234"){
+            alert("Transaction Completed");
+            window.location.href="order.html";
+        }
+        else{
+            alert("Wrong OTP")
+        }
+    }
+}
